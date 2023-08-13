381218e5be81fd38ef9092146af4d7b24e988965
Anorboyev-Xayrullo <anorboyevxayrllo841@gamil.com>
2023-08-13 17:48:22 +0500

    main

--- main.go
+++ main.go
@@ -1,2 +1,127 @@
 package main
 
+import (
+	"fmt"
+)
+
+// func main() {
+// 	var (
+// 		x string = "James Bond"
+// 	y int = 42
+// 	z bool = true
+// 	)
+// 	fmt.Println(x)
+// 	fmt.Println(y)
+// 	fmt.Println(z)
+
+// }
+// 2 misol
+
+// func main()  {
+// 	var (
+// 		x int = 27
+// 		y int = 28
+// 		z int = 26
+
+// 		d = x + y + z
+// 	)
+// 	fmt.Println(d)
+// }
+
+
+// 3 misol
+
+// func main()  {
+// 	var (
+// 		x = 42
+// 		y = "James Bond"
+// 		z = true
+// 	)
+// 	c := fmt.Sprintf("x: %v, y: %v,z: %v",x,y,z) 
+// 	fmt.Println(c)
+// }
+
+// 4 misol
+
+
+// type MyCustomType int
+
+// func main() {
+//   var x MyCustomType
+  
+//   fmt.Println("Значение переменной x:", x)
+
+//   fmt.Printf("Тип переменной x: %T\n", x)
+ 
+//   x = 42
+//   fmt.Println("Новое значение переменной x:", x)
+//   var y int = int(x)
+//   fmt.Println("Значение переменной y:", y)
+//   fmt.Printf("Тип переменной y: %T\n", y)
+// }
+
+// 5 misol
+
+// type opertaion int
+
+
+// func main()  {
+//   var x opertaion
+//   x = 45
+//   fmt.Println(x)	
+// }
+
+
+
+
+// 6 misol
+
+
+// 7 misol
+
+// func main()  {
+// 	var c []int
+// 	for i := 2000 ; i < 3200; i++{
+//      if (i % 7 == 0 && i % 5 != 0){
+//       c = append(c, i)
+// 	 }
+//         for i , num :=range c {
+// 		if i != 0 {
+// 			fmt.Println(", ")
+// 		}
+// 		fmt.Print(num)
+// 		}
+// 	}
+// }
+
+
+// func add (x int ) int {
+// 	if x <= 1{
+// 		return 1
+// 	}else{
+// 		return x * add(x-1)
+// 	}
+// }
+// func main()  {
+// 	fmt.Println(add(8))
+// }
+
+
+
+
+
+
+
+// func main() {
+//    str := "112,33333,4,35"
+//    arr := make([]string, len(str))
+//    for i, r := range str {
+//       arr[i] = string(r)
+//    }
+//    fmt.Println(arr)
+// }
+
+
+
+
+
